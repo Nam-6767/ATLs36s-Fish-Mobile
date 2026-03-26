f01773085cf1892e13792f66b9d213d6d82675b4
Nam-6767 <itichirin126@gmail.com>
2026-03-26 13:47:18 +0700

    Added game constants for ATLs36s Fish in src/utils/constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,23 @@
+// Game Constants for ATLs36s Fish
+
+export const GAME_CONSTANTS = {
+    ISLANDS: {
+        LEGENDARY_RATES: {
+            RATE_1: 7.36,
+            RATE_2: 11.04,
+            RATE_3: 16.56
+        }
+    },
+    FISH_HP: 1000000,
+    ROD_DAMAGE: {
+        MIN: 7500,
+        MAX: 150000
+    },
+    CRIT_CHANCE: 15.67,
+    TENSION_SYSTEM: {
+        MAX: 100,
+        INCREMENT_PER_HIT: 8,
+        DECREMENT_PER_INTERVAL: 2.5,
+        INTERVAL_TIME: 0.5 // in seconds
+    }
+};
